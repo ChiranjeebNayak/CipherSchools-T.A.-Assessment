848d104518b2439ac93e3571ca6f52a792d0abe9
Chiranjeeb Nayak <63226358+ChiranjeebNayak@users.noreply.github.com>
2021-06-17 21:39:51 +0530

    SEcond question

--- question2.cpp
+++ question2.cpp
@@ -3,28 +3,38 @@ using namespace std;
 int solve(int arr[],int n,int p){
 	sort(arr,arr+n);
 	int res=0;
-	
-	
+	for(int i=0;i<n;i++){
+		if(p%arr[i]==0)
+		res++;
+	}
 	return res;
 }
 int main(){
 	int n;
+	cout<<"number of gadget = ";
 	cin>>n;
+	cout<<"Price of gadgets = ";
 	int arr[n];
 	for(int i=0;i<n;i++){
 		cin>>arr[i];
 	}
 	sort(arr,arr+n);
-	cout<<"Price unit = ";
-	int p;
-	cin>>p;
-	int item;
-	cout<<"Robot says items = ";
-	cin>>item;
-	int res= solve(arr,n,p);
-	if(item == res)
-	cout<<"Right\n";
-	else
-	cout<<"Wrong\n";
+	cout<<"query = ";
+	int q;
+	cin>>q;
+	while(q--){
+		cout<<"Price unit = ";
+		int p;
+		cin>>p;
+		int item;
+		cout<<"Robot says items = ";
+		cin>>item;
+		int res= solve(arr,n,p);
+		if(item == res)
+		cout<<"Right\n";
+		else
+		cout<<"Wrong\n";	
+	}
+	
 	return 0;
 }
